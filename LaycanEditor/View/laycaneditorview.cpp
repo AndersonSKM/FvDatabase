f2e7b4e2166fd993f8947d0e63fbfa4b64ab3814
Anderson Macedo <anderson.krs95@gmail.com>
2016-05-15 18:31:50 -0300

    Create CRUD options

--- LaycanEditor/View/laycaneditorview.cpp
+++ LaycanEditor/View/laycaneditorview.cpp
@@ -22,6 +22,36 @@ LaycanEditorView::~LaycanEditorView()
     delete model;
 }
 
+void LaycanEditorView::setMode(const EditorModes &mode)
+{
+    //Configure window by mode
+    switch (mode) {
+        case View:
+            setEditButtonsEnabled(false);
+            setActionsButtonsEnabled(true);
+            setEditsEnabled(false);
+        break;
+
+        case Edit:
+            setEditButtonsEnabled(true);
+            setActionsButtonsEnabled(false);
+            setEditsEnabled(true);
+        break;
+
+        case New:
+            setEditButtonsEnabled(true);
+            setActionsButtonsEnabled(false);
+            setEditsEnabled(true);
+        break;
+    }
+    m_mode = mode;
+}
+
+EditorModes LaycanEditorView::mode() const
+{
+    return m_mode;
+}
+
 void LaycanEditorView::showEvent(QShowEvent *event)
 {
     QMainWindow::showEvent(event);
@@ -53,6 +83,8 @@ void LaycanEditorView::showEvent(QShowEvent *event)
 
     //Filter shortcut keys
     ui->treeMigrations->installEventFilter(this);
+
+    setMode(View);
     m_initialized = true;
 }
 
@@ -69,10 +101,34 @@ void LaycanEditorView::abort(const QString &msg)
     }
 }
 
+void LaycanEditorView::setEditsEnabled(const bool value)
+{
+    ui->edDescription->setEnabled(value);
+    ui->edVersion->setEnabled(value);
+    ui->tbDownVersion->setEnabled(value);
+    ui->tbUpVersion->setEnabled(value);
+    ui->lbDescription->setEnabled(value);
+    ui->lbVersion->setEnabled(value);
+
+    ui->treeMigrations->setEnabled(!value);
+}
+
+void LaycanEditorView::setActionsButtonsEnabled(const bool value)
+{
+    ui->btnEdit->setEnabled(value);
+    ui->btnNew->setEnabled(value);
+    ui->btnDelete->setEnabled(value);
+}
+
+void LaycanEditorView::setEditButtonsEnabled(const bool value)
+{
+    ui->btnSave->setEnabled(value);
+    ui->btnCancel->setEnabled(value);
+}
+
 void LaycanEditorView::readFile(const QDomDocument &document)
 {
     //Parse Xml File
-
     QStandardItem *root = new QStandardItem("Migrations");
     model->appendRow(root);
 
@@ -93,14 +149,16 @@ void LaycanEditorView::readFile(const QDomDocument &document)
         QDomElement upVersion = migration.firstChildElement("UpVersion");
         QDomNode sqlUp = upVersion.firstChild();
         if (sqlUp.nodeType() == 8) {
-            items.append(new QStandardItem(sqlUp.nodeValue()));
+            QString valueUp = sqlUp.nodeValue().remove("-->").remove("<--");
+            items.append(new QStandardItem(valueUp));
         }
 
         //Down SQL in comment node
         QDomElement downVersion = migration.firstChildElement("DownVersion");
         QDomNode sqlDown = downVersion.firstChild();
         if (sqlDown.nodeType() == 8) {
-            items.append(new QStandardItem(sqlDown.nodeValue()));
+            QString valueDown = sqlDown.nodeValue().remove("-->").remove("<--");
+            items.append(new QStandardItem(valueDown));
         }
 
         //Append row in all migrations
@@ -121,7 +179,7 @@ void LaycanEditorView::setInitialized(bool initialized)
 void LaycanEditorView::on_treeMigrations_clicked(const QModelIndex &index)
 {
     int row = index.row();
-    QModelIndex parent = index.parent();
+    QModelIndex parent = index.parent();   
     QString version = model->data(model->index(row, 0, parent), Qt::EditRole).toString();
     QString description = model->data(model->index(row, 1, parent), Qt::EditRole).toString();
     QString upVersionSQL = model->data(model->index(row, 2, parent), Qt::EditRole).toString();
@@ -144,6 +202,7 @@ bool LaycanEditorView::eventFilter(QObject *obj, QEvent *event)
             if (index.isValid()) {
                 tree->setCurrentIndex(index);
                 tree->clicked(index);
+                return true;
             }
         }
 
@@ -152,12 +211,24 @@ bool LaycanEditorView::eventFilter(QObject *obj, QEvent *event)
             if (index.isValid()) {
                 tree->setCurrentIndex(index);
                 tree->clicked(index);
+                return true;
             }
         }
-
-        return true;
     }
 
     // standard event processing
     return QObject::eventFilter(obj, event);
+}
+
+void LaycanEditorView::on_btnEdit_clicked()
+{
+    setMode(Edit);
+}
+
+void LaycanEditorView::on_btnCancel_clicked()
+{
+    if (QMessageBox::question(this,"","Discart Changes ?") == QMessageBox::No)
+        return;
+
+   setMode(View);
 }
--- LaycanEditor/View/laycaneditorview.h
+++ LaycanEditor/View/laycaneditorview.h
@@ -16,6 +16,12 @@ namespace Ui {
 class laycaneditorview;
 }
 
+enum EditorModes {
+    New  = 1,
+    Edit = 2,
+    View = 3,
+};
+
 class LaycanEditorView : public QMainWindow
 {
     Q_OBJECT
@@ -27,11 +33,18 @@ public:
     bool isInitialized() const;
     void setInitialized(bool initialized);
 
+    EditorModes mode() const;
+    void setMode(const EditorModes &mode);
+
 public slots:
     void showEvent(QShowEvent* event);
 
 private slots:
     void on_treeMigrations_clicked(const QModelIndex &index);
+
+    void on_btnEdit_clicked();
+
+    void on_btnCancel_clicked();
 
 protected:
     bool eventFilter(QObject *obj, QEvent *event);
@@ -40,11 +53,15 @@ private:
     void readFile(const QDomDocument &document);
     void writeFile();
     void abort(const QString &msg = "");
+    void setEditsEnabled(const bool value);
+    void setActionsButtonsEnabled(const bool value);
+    void setEditButtonsEnabled(const bool value);
 
+    bool m_initialized;
+    EditorModes m_mode;
     QFile m_xmlFile;
     QStandardItemModel *model;
     Ui::laycaneditorview *ui;
-    bool m_initialized;
 };
 
 #endif // LAYCANEDITORVIEW_H
